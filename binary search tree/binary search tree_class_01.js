fd3ad1a2629a1340fa2766e3230e20bc88d9e0fe
Phyf3 <phade075@gmail.com>
2022-08-19 12:58:45 +0100

    BFS and deletion

--- binary search tree/binary search tree_class_01.js
+++ binary search tree/binary search tree_class_01.js
@@ -103,13 +103,86 @@ class BinarySearchTree{
             console.log(root.value)
         }
     }
+
+    //Traversal -> BFS
+
+    levelOrder() {
+        //**try the optimised queue implementation in the queue folder?
+        const Queue = [];
+        Queue.push(this.root)//enqueue
+        while(Queue.length) {
+            let curr = Queue.shift() //removes first elemnt of the array --> dequeue
+            console.log(curr.value)
+
+            if(curr.left) {
+                Queue.push(curr.left)
+            }
+
+            if(curr.right) {
+                Queue.push(curr.right)
+            }
+        } 
+    }
+
+
+    min(root) {
+        //Fn just keeps going to the left till there's no more left value 
+        //...then we return the value of that node since it's the leftmost
+        if(!root.left) {
+            return root.value
+        } else {
+            return this.min(root.left)
+        }
+    }
+
+    max(root) {
+        if(!root.right) {
+            return root.value
+        } else {
+            return this.max(root.right)
+        }
+    }
+
+    delete(value) {
+        this.root = this.deleteNode(this.root, value)
+    }
+
+    deleteNode(root, value) {
+        if(root === null) {
+            return root
+        }
+
+        // 'if' and 'else if' methods are used to find the value from the tree recursively
+        //when it's found that value === root.value, Fn is 'else' block runs
+        if(value < root.value) {
+            root.left = this.deleteNode(root.left, value)
+        } else if (value > root.value) {
+            root.right = this.deleteNode(root.right, value)
+        } else {
+            //node is a leaf node
+            if(!root.left && !root.right) {
+                return null
+            }
+
+            //'if' & 'else if' --> when node has only one child node
+            // 'else' block --> when node has both children.
+            if(!root.left) {
+                return root.right
+            } else if(!root.right) {
+                return root.left
+            }
+            root.value = this.min(root.right)
+            root.right = this.deleteNode(root.right, root.left)
+        }
+        return root
+    }
 }
 
 const bst = new BinarySearchTree()
 console.log(`Is the BST empty? ${bst.isEmpty()}`)  //Expected output : true
 
 console.log("\n\t BST Insertion")
-bst.insert(10)
+bst.insert(10) 
 bst.insert(5)
 bst.insert(15)
 bst.insert(3)
@@ -119,6 +192,8 @@ bst.insert(11)
 
 console.log(`Is the BST still empty? ${bst.isEmpty()}`) //Expected ouput : false
 
+/*
+    // SEARCH
 console.log("\n\t BST Search_with root")
 console.log(`is "5" in the binary tree? ${bst.search(bst.root,5)}`) //Expected output : true
 console.log(`is "3" in the binary tree? ${bst.search(bst.root,3)}`) //Expected output : true
@@ -129,6 +204,10 @@ console.log(`is "5" in the binary tree? ${bst.searchWithLoop(5)}`) //Expected ou
 console.log(`is "3" in the binary tree? ${bst.searchWithLoop(3)}`) //Expected output : true
 console.log(`is "50" in the binary tree? ${bst.searchWithLoop(50)}`) //Expected output : false
 
+*/
+
+/*
+    // DEPTH FIRST SEARCH
 console.log("\n Depth First Search")
 console.log("\t PreOrder")
 bst.preOrder(bst.root)
@@ -138,6 +217,28 @@ bst.inOrder(bst.root)
 
 console.log("\t postOrder")
 bst.postOrder(bst.root)
+
+    // BREADTH FIRST SEARCH
+console.log("\t levelOrder")
+bst.levelOrder(bst.root)
+*/
+
+    // MIN & MAX
+console.log("\n Min & Max")
+console.log(`Min value of the tree is ${bst.min(bst.root)}`)
+console.log(`Max value in the tree is ${bst.max(bst.root)}`)
+
+    //DELETION
+console.log("\n Delete")
+let toDelete = 3
+console.log(`\t Before deleting ${toDelete}`)
+bst.levelOrder(bst.root)
+bst.delete(toDelete)
+console.log(`\n\t Removed ${toDelete}`)
+bst.levelOrder(bst.root)
+
+
+
 
 
 
